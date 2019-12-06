80bbb1909caac8eb932e3e2969f0e7b3d760894e
Lázaro Nazário <jose.lazaro.n.silva@accenture.com>
2019-12-06 18:54:07 -0300

    inicial

---
+++ src/test/java/br/com/costaduarteempilhadeiras/cadastromaquina/CadastroMaquinaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.costaduarteempilhadeiras.cadastromaquina;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CadastroMaquinaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
